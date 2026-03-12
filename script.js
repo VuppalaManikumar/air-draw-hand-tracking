5976bc9dc2fa4f1bd0b28f9be65995e332a24bba
Manikumar VUPPALA <manikumarvv624@gmail.com>
2026-03-12 17:10:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const videoElement = document.createElement("video");
-const canvasElement = document.getElementById("canvas");
+const videoElement = document.getElementById("video");
+const canvasElement = document.getElementById("drawCanvas");
 const canvasCtx = canvasElement.getContext("2d");
 
 let drawing = false;
@@ -7,91 +7,75 @@ let lastX = 0;
 let lastY = 0;
 
 canvasCtx.lineWidth = 5;
-canvasCtx.lineCap = "round";
 canvasCtx.strokeStyle = "red";
+canvasCtx.lineCap = "round";
 
-async function setupCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video: true
-    });
-
-    videoElement.srcObject = stream;
-    videoElement.play();
+function drawLine(x,y){
+canvasCtx.beginPath();
+canvasCtx.moveTo(lastX,lastY);
+canvasCtx.lineTo(x,y);
+canvasCtx.stroke();
 }
 
-function drawLine(x, y) {
-    canvasCtx.beginPath();
-    canvasCtx.moveTo(lastX, lastY);
-    canvasCtx.lineTo(x, y);
-    canvasCtx.stroke();
+function onResults(results){
+
+canvasCtx.drawImage(results.image,0,0,640,480);
+
+if(results.multiHandLandmarks){
+
+for(const landmarks of results.multiHandLandmarks){
+
+const index = landmarks[8];
+
+const x = index.x * 640;
+const y = index.y * 480;
+
+if(drawing){
+drawLine(x,y);
 }
 
-function onResults(results) {
+lastX = x;
+lastY = y;
 
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-    canvasCtx.drawImage(
-        results.image,
-        0,
-        0,
-        canvasElement.width,
-        canvasElement.height
-    );
-
-    if (results.multiHandLandmarks) {
-
-        for (const landmarks of results.multiHandLandmarks) {
-
-            const indexFinger = landmarks[8];
-
-            const x = indexFinger.x * canvasElement.width;
-            const y = indexFinger.y * canvasElement.height;
-
-            if (drawing) {
-                drawLine(x, y);
-            }
-
-            lastX = x;
-            lastY = y;
-        }
-    }
 }
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Control") {
-        drawing = true;
-    }
+}
+
+}
+
+document.addEventListener("keydown",function(e){
+if(e.key==="Control"){
+drawing=true;
+}
 });
 
-document.addEventListener("keyup", (event) => {
-    if (event.key === "Control") {
-        drawing = false;
-    }
+document.addEventListener("keyup",function(e){
+if(e.key==="Control"){
+drawing=false;
+}
 });
 
 const hands = new Hands({
-    locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    }
+locateFile: (file) => {
+return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}
 });
 
 hands.setOptions({
-    maxNumHands: 1,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.7,
-    minTrackingConfidence: 0.7
+maxNumHands:1,
+modelComplexity:1,
+minDetectionConfidence:0.7,
+minTrackingConfidence:0.7
 });
 
 hands.onResults(onResults);
 
-const camera = new Camera(videoElement, {
-    onFrame: async () => {
-        await hands.send({ image: videoElement });
-    },
-    width: 640,
-    height: 480
+const camera = new Camera(videoElement,{
+onFrame: async () => {
+await hands.send({image: videoElement});
+},
+width:640,
+height:480
 });
 
-setupCamera().then(() => {
-    camera.start();
-});
+camera.start();
